ee15f0249ec3b71249bbdbed0f13b01d1038259a
kruser <kruser@yandex.ru>
2023-04-18 14:59:12 +0500

    add debug-toolbar

--- selfstorage/urls.py
+++ selfstorage/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from storage.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
