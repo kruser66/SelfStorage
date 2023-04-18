cfb627f5ab15ab643bc162a77627ce2ca3e03b3e
Rikoze777 <spawn00007@gmail.com>
2023-04-18 19:17:21 +0500

    register models

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from models import Client,  Order
 
-# Register your models here.
+
+class OrderItemsInline(admin.TabularInline):
+    model = Order
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    search_fields = [
+        'email',
+        'username',
+        'password',
+        'phonenumber',
+    ]
+
+    inlines = [OrderItemsInline, ]
