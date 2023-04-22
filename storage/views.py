b0b28f33489d6d80e4024f87c129270844f884b1
kruser <kruser@yandex.ru>
2023-04-22 23:12:51 +0500

    fixed PEP-8

--- storage/views.py
+++ storage/views.py
@@ -2,24 +2,20 @@ from random import choice
 
 from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.core.exceptions import ValidationError
 from django.core.mail import send_mail
 from django.db.models import Count, F, Min, Q
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
 from storage.models import Storage
 from storage.payments import create_payment, get_payment_status
 
-from .forms import AccountForm, CustomUserCreationForm, LoginForm
 from .models import Box, Order, Rental, User
 
 from storage.sendmail import send_email
 
-def user_login(request):
 
+def user_login(request):
     if request.method == "POST":
         email=request.POST['EMAIL']
         password=request.POST['PASSWORD']
@@ -31,7 +27,6 @@ def user_login(request):
             return redirect('/my-rent')
         
         return redirect("/")
-
 
 
 def user_register(request):
@@ -78,78 +73,10 @@ def edit(request):
         user.first_name=request.POST['FIRSTNAME_EDIT']
         user.last_name=request.POST['LASTNAME_EDIT']
         user.phonenumber=request.POST['PHONE_EDIT']
-        # user.password=request.POST['PASSWORD_EDIT']
         user.save()
     return redirect("/my-rent")    
-    
-    
-
-# ################## LOGIN ############################
-    
-# def register_user(request, *args, **kwargs):
-#     user = request.user
-#     if user.is_authenticated:
-#         return HttpResponse(f"Вы уже зарегистрированы как {user.email}")
-
-#     if request.method == "POST":
-#         form = CustomUserCreationForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             user = authenticate(
-#                 request,
-#                 username=form.cleaned_data.get("email"),
-#                 password=form.cleaned_data.get("password1"),
-#             )
-#             login(request, user)
-#             destination = kwargs.get("next")
-#             if destination:
-#                 return redirect(destination)
-#             return redirect("/")
-#     else:
-#         form = CustomUserCreationForm()
-#     return render(request, "users/register.html", {"form": form})
 
 
-# def login_user(request):
-#     if request.method == "POST":
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             print(cd)
-#             user = authenticate(username=cd["email"], password=cd["password"])
-#             if user is not None:
-#                 if user.is_active:
-#                     login(request, user)
-#                     return redirect("/")
-#                 else:
-#                     form.add_error(None, ValidationError("Этот аккаунт отключен"))
-#             else:
-#                 form.add_error(None, ValidationError("Неверный email или пароль."))
-#     else:
-#         form = LoginForm()
-#     return render(request, "users/login.html", {"form": form})
-
-
-# @login_required(login_url="users:login")
-# def account(request):
-#     user = request.user
-#     paid_orders = user.orders.filter(payments__is_paid=True)
-#     if request.method == "POST":
-#         form = AccountForm(request.POST)
-#         if form.is_valid():
-#             user.email = form.cleaned_data["email"]
-#             user.phonenumber = form.cleaned_data["phonenumber"]
-#             user.phonenumber = form.cleaned_data["phonenumber"]
-#             user.set_password(form.cleaned_data["password"])
-#             user.save()
-#     else:
-#         form = AccountForm()
-
-#     context = {"user": user, "paid_orders": paid_orders, "form": form}
-#     return render(request, "account.html", context)
-
-
-# ################### PAGES ###########################
 def index(request):
     storages = Storage.objects.all().prefetch_related('boxes') \
         .annotate(box_min_price=Min(F('boxes__price'))) \
@@ -165,31 +92,15 @@ def faq(request):
     return render(request, 'faq.html')
 
 
-# @login_required(login_url="login")
 def my_rent(request):
     user = request.user
-    if user.is_authenticated:
-        print(user.id)
-        
-        rentals = Rental.objects.filter(client=user, box__busy=True)
-        print(rentals)
-        
+    if user.is_authenticated:       
+        rentals = Rental.objects.filter(client=user, box__busy=True)   
+             
         return render(request, 'my-rent.html', {'rentals': rentals})
+    
     else:
         return redirect("/") 
-    # paid_orders = user.orders.filter(status='PAID')
-    # if request.method == "POST":
-    #     form = AccountForm(request.POST)
-    #     if form.is_valid():
-    #         user.email = form.cleaned_data["email"]
-    #         user.phonenumber = form.cleaned_data["phonenumber"]
-    #         user.set_password(form.cleaned_data["password"])
-    #         user.save()
-    # else:
-    #     form = AccountForm()
-
-    # context = {"user": user, "paid_orders": paid_orders, "form": form}
-    # return render(request, 'my-rent.html', context)
 
 
 def boxes(request):
@@ -197,22 +108,6 @@ def boxes(request):
         .annotate(box_min_price=Min(F('boxes__price'))) \
         .annotate(box_count=Count('boxes')) \
         .annotate(box_free=Count('boxes', filter=Q(boxes__busy=False)))
-
-    # boxes = []
-    # all_storages = Storage.objects.prefetch_related()
-    # for storage in all_storages:
-    #     free_boxes = Box.objects.filter(storage=storage, busy=False).prefetch_related()
-    #     for box in free_boxes:
-    #         box_stats = {
-    #             'id': box.id,
-    #             'floor': box.floor,
-    #             'volume': int(box.volume),
-    #             'dimension': box.dimension,
-    #             'price': box.price,
-    #         }
-    #         boxes.append(box_stats)
-    
-    # boxes = sorted(boxes, key=lambda x: x['price']) 
     
     return render(
         request,
